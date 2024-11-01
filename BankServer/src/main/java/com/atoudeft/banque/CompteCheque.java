215786a7ebf54ea05505e13f7b44f8c19ecfbd9d
Yasser Manouzi <79117423+YasserManouzi@users.noreply.github.com>
2024-11-01 14:59:35 -0400

    Création classe CompteCheque
    Création de la classe CompteCheque dérivant de la classe CompteBancaire avec tous ses méthodes impléementées.

---
+++ BankServer/src/main/java/com/atoudeft/banque/CompteCheque.java
@@ -0,0 +1,45 @@
+package com.atoudeft.banque;
+
+public class CompteCheque extends CompteBancaire{
+    /**
+     * Crée un compte bancaire.
+     *
+     * @param numero numéro du compte
+     * @param type   type du compte
+     */
+    public CompteCheque(String numero, TypeCompte type) {
+        super(numero, type);
+    }
+
+    @Override
+    public boolean crediter(double montant) {
+        if(montant > 0) {
+            double solde = getSolde();
+            solde += montant;
+            return true;
+        }
+            return false;
+
+
+    }
+
+    @Override
+    public boolean debiter(double montant) {
+        if(montant > 0) {
+            double solde = getSolde();
+            solde -= montant;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean payerFacture(String numeroFacture, double montant, String description) {
+        return false;
+    }
+
+    @Override
+    public boolean transferer(double montant, String numeroCompteDestinataire) {
+        return false;
+    }
+}
