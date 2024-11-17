2028fd182340eef3da55d83189fdc6bfe2c81954
Yasser Manouzi <79117423+YasserManouzi@users.noreply.github.com>
2024-11-17 15:08:41 -0500

    Modification getCompteClient classe Banque
    Modification du code de la méthode getCompteClient de la classe Banque pour régler le problème d'un compte client toujours null

--- BankServer/src/main/java/com/atoudeft/banque/Banque.java
+++ BankServer/src/main/java/com/atoudeft/banque/Banque.java
@@ -23,13 +23,14 @@ public class Banque implements Serializable {
      * @return le compte-client s'il a été trouvé. Sinon, retourne null
      */
     public CompteClient getCompteClient(String numeroCompteClient) {
-        CompteClient cpt = new CompteClient(numeroCompteClient,"");
-        int index = this.comptes.indexOf(cpt);
-        if (index != -1)
-            return this.comptes.get(index);
-        else
-            return null;
+        for (CompteClient compte : comptes) {
+            if (compte.getNumero().equals(numeroCompteClient)) {
+                return compte;
+            }
+        }
+        return null;
     }
+
 
     /**
      * Vérifier qu'un compte-bancaire appartient bien au compte-client.
